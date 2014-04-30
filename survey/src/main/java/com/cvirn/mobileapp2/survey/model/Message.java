39ac0af4e958a7f7f04269949c570ac7d72c788e
jankocvirn <jankocvirn@gmail.com>
2014-04-30 16:00:10 +0200

    v3

--- survey/src/main/java/com/cvirn/mobileapp2/survey/model/Message.java
+++ survey/src/main/java/com/cvirn/mobileapp2/survey/model/Message.java
@@ -4,4 +4,11 @@ package com.cvirn.mobileapp2.survey.model;
  * Created by janko on 4/30/14.
  */
 public class Message {
+
+    protected String id;
+    protected String poi;
+    protected String status;
+    protected String due_date;
+
+
 }
